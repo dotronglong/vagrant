b9a4a51bf565c7a57f4547ce5a67afb692d9fe8d
admin.long.do <admin.long.do@LAZ-VN-L-M-1162.local>
2017-04-21 11:06:55 +0700

    Update scripts

--- lamp/provision.sh
+++ lamp/provision.sh
@@ -1,90 +1,76 @@
 #!/bin/sh
+say() { echo >&1 -e ":: $*"; }
+info() { echo >&1 -e ":: \033[01;32m$*\033[00m"; }
+warn() { echo >&2 -e ":: \033[00;31m$*\033[00m"; }
+die() { echo >&2 -e ":: \033[00;31m$*\033[00m"; exit 1; }
+null() { echo >/dev/null; }
 
-# Disable SELinux
-cat <<EOT > /etc/sysconfig/selinux
-SELINUX=disabled
-SELINUXTYPE=targeted
-EOT
+function setup() {
+  # Disable SELinux
+  info "Set Up SELinux"
+  cp -pr /vagrant/ops/etc/sysconfig/selinux /etc/sysconfig/selinux
 
-# Install necessary tools
-yum install -y wget vim git unzip
+  # Install necessary tools
+  command="yum install -y wget vim git unzip"
+  info $command && eval $command
 
-# Install apache-2.4.*
-curl -SLo /etc/yum.repos.d/epel-httpd24.repo https://repos.fedorapeople.org/repos/jkaluza/httpd24/epel-httpd24.repo
-yum install -y httpd httpd-devel httpd-tools
-systemctl enable httpd
-systemctl enable httpd
-mkdir /var/www/tools
+  # Repos
+  cp -pr /vagrant/ops/yum.repos.d/* /etc/yum.repos.d/
+}
 
-# Install MariaDB-10.1
-yum remove -y mariadb-*
-cat <<EOT > /etc/yum.repos.d/MariaDB.repo
-# MariaDB 10.1 CentOS repository list - created 2017-04-20 03:51 UTC
-# http://downloads.mariadb.org/mariadb/repositories/
-[mariadb]
-name = MariaDB
-baseurl = http://yum.mariadb.org/10.1/centos7-amd64
-gpgkey=https://yum.mariadb.org/RPM-GPG-KEY-MariaDB
-gpgcheck=1
-EOT
-yum install -y MariaDB-server MariaDB-client
-systemctl enable mariadb
-systemctl start mariadb
-mysql_secure_installation
-/vagrant/db.sh init
+function install_httpd() {
+  info "Install Apache"
+  command="yum install -y httpd httpd-devel httpd-tools"
+  info $command && eval $command
 
-# Install php-7.1
-rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
-yum install -y --skip-broken php71w-* mod_php71w
-cat <<EOT > /etc/httpd/conf.modules.d/10-php.conf
-<IfModule prefork.c>
-  LoadModule php7_module modules/libphp7.so
-</IfModule>
+  cp -pr /vagrant/ops/httpd/conf/httd.conf
+  cp -pr /vagrant/ops/httpd/conf.d/* /etc/httpd/conf.d/
+  systemctl enable httpd
+  systemctl enable httpd
+  mkdir /var/www/tools
+}
 
-<IfModule !prefork.c>
-  LoadModule php7_module modules/libphp7-zts.so
-</IfModule>
-EOT
-cat <<EOT > /etc/httpd/conf.d/php.conf
-<IfModule mime_module>
-  AddType application/x-httpd-php .php
-</IfModule>
-EOT
-cat <<EOT > /etc/php.d/core.ini
-display_errors = On
-error_reporting = E_ALL
-error_log = /var/log/php.log
-max_input_time = 180
-max_execution_time = 120
-memory_limit = 1024
-upload_max_filesize = 200M
-post_max_size = 300M
-EOT
+function install_mariadb() {
+  info "Install MariaDB"
+  yum remove -y mariadb-*
+  rpm --import https://yum.mariadb.org/RPM-GPG-KEY-MariaDB
+  command="yum install -y MariaDB-server MariaDB-client"
+  info $command && eval $command
+  systemctl enable mariadb
+  systemctl start mariadb
+  # mysql_secure_installation
+  # /vagrant/db.sh init
+}
 
-# Install phpMyAdmin 4.7.0
-curl -SLO https://files.phpmyadmin.net/phpMyAdmin/4.7.0/phpMyAdmin-4.7.0-english.zip
-unzip phpMyAdmin-4.7.0-english.zip
-rm -rf phpMyAdmin-4.7.0-english.zip
-cat <<EOT > phpMyAdmin-4.7.0-english/config.inc.php
-<?php
-$cfg['blowfish_secret'] = 'Dplayi6FaWtbQp4YUOi2peL91TkBTP3j';
-$i = 0;
-$i++;
-$cfg['Servers'][$i]['auth_type'] = 'cookie';
-$cfg['Servers'][$i]['host'] = 'localhost';
-$cfg['Servers'][$i]['compress'] = false;
-$cfg['Servers'][$i]['AllowNoPassword'] = false;
-EOT
-mv phpMyAdmin-4.7.0-english /var/www/tools/phpmyadmin
-cat <<EOT > /etc/httpd/conf.d/alias.conf
-Alias /phpmyadmin /var/www/tools/phpmyadmin
-EOT
-cat <<EOT > /etc/httpd/conf.d/phpmyadmin.conf
-<Directory "/var/www/tools/phpmyadmin">
-  DirectoryIndex index.php
-  AllowOverride FileInfo AuthConfig Limit Indexes
-  Options MultiViews Indexes SymLinksIfOwnerMatch IncludesNoExec
-  Require method GET POST OPTIONS
-</Directory>
-EOT
+function install_php() {
+  info "Install PHP"
+  rpm -Uvh /vagrant/ops/rpm/epel-release-latest-7.noarch.rpm
+  rpm -Uvh /vagrant/ops/rpm/webtatic-release.rpm
+  command="yum install -y --skip-broken php71w-* mod_php71w"
+  info $command && eval $command
+  cp -pr /vagrant/ops/php/conf.d/10-php.conf /etc/httpd/conf.modules.d/10-php.conf
+  cp -pr /vagrant/ops/php/php.d/* /etc/php.d/
+}
+
+function install_phpmyadmin() {
+  info "Install phpMyAdmin"
+  curl -SLO https://files.phpmyadmin.net/phpMyAdmin/4.7.0/phpMyAdmin-4.7.0-english.zip
+  unzip phpMyAdmin-4.7.0-english.zip
+  rm -rf phpMyAdmin-4.7.0-english.zip
+  mv phpMyAdmin-4.7.0-english phpmyadmin
+  cp -pr /vagrant/ops/phpmyadmin/config.inc.php phpmyadmin/config.inc.php
+  mv phpmyadmin /var/www/tools/phpmyadmin
+
+  echo "Alias /phpmyadmin /var/www/tools/phpmyadmin" >> /etc/httpd/conf.d/alias.conf
+  cp -pr /vagrant/ops/phpmyadmin/phpmyadmin.conf /etc/httpd/conf.d/phpmyadmin.conf
+}
+
+function all() {
+  setup
+  install_httpd
+  install_mariadb
+  install_php
+  install_phpmyadmin
+  info "DONE!!!"
+}
+$*
